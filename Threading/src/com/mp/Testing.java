7a171a6ac37ad5837c5f1780a471e9930f3f6f63
ManojPatidar007 <manojpatidar315@gmail.com>
2018-09-15 08:10:19 -0700

    threading

---
+++ Threading/src/com/mp/Testing.java
@@ -0,0 +1,10 @@
+package com.mp;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Threading/src/com/mp/Thread1.java
@@ -0,0 +1,11 @@
+package com.mp;
+
+public class Thread1 implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
